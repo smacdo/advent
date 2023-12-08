5c486c23c6e8291a13ba568afe765259259d9381
Scott MacDonald <scott@smacdo.com>
2023-12-08 15:18:00 -0800

    solve day 8 part 2

--- day8.py
+++ day8.py
@@ -4,6 +4,7 @@ import re
 import unittest
 
 from utils import AdventDaySolver, AdventDayTestCase, init_logging
+from math import lcm
 
 
 class Node:
@@ -40,8 +41,48 @@ def parse_input(input_lines):
     return (commands, nodes)
 
 
+def solve_path(commands, nodes, start_node_name, end_node_name):
+    def is_target(node, end_node_name):
+        if end_node_name:
+            return node.name == end_node_name
+        else:
+            return node.name.endswith("Z")
+
+    n = nodes[start_node_name]
+    i = 0
+    steps = 0
+
+    while not is_target(n, end_node_name):
+        # Infinite loop check:
+        # if steps > 10:
+        #    raise Exception("solver hit too many steps - infinite loop?")
+
+        # Perform requested move.
+        command = commands[i]
+        next_node_name = n.go(command)
+
+        # logging.debug(
+        #    f"{command}: {n.name}, {n.left}, {n.right} -> {next_node_name} step {steps}"
+        # )
+        assert next_node_name in nodes
+
+        n = nodes[next_node_name]
+        steps += 1
+
+        # Move to next command.
+        i += 1
+        if i >= len(commands):
+            i = 0
+
+    return steps
+
+
 class Solver(
-    AdventDaySolver, day=8, year=2023, name="Haunted Wasteland", solution=(14681, None)
+    AdventDaySolver,
+    day=8,
+    year=2023,
+    name="Haunted Wasteland",
+    solution=(14681, 14321394058031),
 ):
     def __init__(self, input):
         super().__init__(input)
@@ -57,61 +98,31 @@ class Solver(
 
     def solve_1(self):
         assert "AAA" in self.nodes
-        n = self.nodes["AAA"]
-        i = 0
-        steps = 0
-
-        while n.name != "ZZZ":
-            # Infinite loop check:
-            # if steps > 10:
-            #    raise Exception("solver hit too many steps - infinite loop?")
-
-            # Perform requested move.
-            command = self.commands[i]
-            next_node_name = n.go(command)
-            # logging.debug(
-            #    f"{command}: {n.name}, {n.left}, {n.right} -> {next_node_name} step {steps}"
-            # )
-            assert next_node_name in self.nodes
-
-            n = self.nodes[next_node_name]
-            steps += 1
-
-            # Move to next command.
-            i += 1
-            if i >= len(self.commands):
-                i = 0
-
-        return steps
+        return solve_path(self.commands, self.nodes, "AAA", "ZZZ")
 
     def solve_2(self):
-        nodes = [self.nodes[n] for n in self.nodes if n.endswith("A")]
+        # This one is a bit nasty. My initial solution was to copy the solution
+        # to part #1, and modify it work for multiple nodes at once. Turns out
+        # the input set has cycles that (in theory) eventually sync up to put
+        # all the nodes on target BUT the number of steps to get there is huge.
+        #
+        # Another solution is to calculate the number of steps each node needs
+        # from its starting location, and then find the lowest common multiple
+        # which gives us the answer without having to simulate every step. This
+        # solution is similiar to problems in AoC question in part years. A more
+        # generic solution would be to use the Chinese remainder theorem (CRT).
+        #
+        # Using LCM works because the input is constructed in a specific manner
+        # such that the number of nodes BEFORE the "head" of the cycle is
+        # exactly same as the number of nodes IN the cycle.
+        start_nodes = [self.nodes[n] for n in self.nodes if n.endswith("A")]
+        end_nodes = [
+            solve_path(self.commands, self.nodes, n.name, None) for n in start_nodes
+        ]
 
-        i = 0
-        steps = 0
+        logging.debug(f"ends: {end_nodes}")
 
-        while any(n for n in nodes if not n.name.endswith("Z")):
-            # Infinite loop check:
-            if steps > 1000000:
-                raise Exception("solver hit too many steps - infinite loop?")
-
-            # Perform requested move.
-            command = self.commands[i]
-
-            for n_i, n in enumerate(nodes):
-                next_node_name = n.go(command)
-                assert next_node_name in self.nodes
-
-                nodes[n_i] = self.nodes[next_node_name]
-
-            steps += 1
-
-            # Move to next command.
-            i += 1
-            if i >= len(self.commands):
-                i = 0
-
-        return steps
+        return lcm(*end_nodes)
 
 
 class Tests(AdventDayTestCase):
@@ -167,8 +178,7 @@ XXX = (XXX, XXX)"""
     def test_real_input(self):
         s = self._create_real_solver().solve()
         self.assertEqual(14681, s[0])
-        self.assertEqual(None, s[1])
-        pass
+        self.assertEqual(14321394058031, s[1])
 
 
 if __name__ == "__main__":
