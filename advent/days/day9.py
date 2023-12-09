33f554997d166a6e85af53ec5cc59466dff0143c
Scott MacDonald <scott@smacdo.com>
2023-12-08 21:46:51 -0800

    day 9 solution

---
+++ advent/days/day9.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+import logging
+import unittest
+
+from advent.utils import (
+    AdventDaySolver,
+    AdventDayTestCase,
+    init_logging,
+    run_tests_for_solver,
+)
+
+
+def parse_inputs(input_lines):
+    entries = []
+    for line in input_lines:
+        entries.append([int(s) for s in line.split()])
+
+    return entries
+
+
+def reduce(entry):
+    # reduce the entry to zero, and return an array containing the last col
+    # of each row starting from the bottom (but not including the zero).
+    # logging.debug(f"REDUCE: {entry}")
+    first_cols = []
+    placeholder_sum = 0
+    steps = 0
+
+    while not all(x == 0 for x in entry):
+        if steps > 100:
+            raise Exception("debug tripwire")
+
+        steps += 1
+
+        # save the first column
+        first_cols.append(entry[0])
+
+        # reduce the row
+        for i in range(0, len(entry) - 1):
+            entry[i] = entry[i + 1] - entry[i]
+
+        # Remove last element from entry, and use that value to extrapolate
+        # forwards.
+        last_element = entry.pop()
+        placeholder_sum += last_element
+
+    # Extrapolate backwards.
+    first_cols.reverse()
+    new_first = 0
+
+    for x in first_cols:
+        new_first = x - new_first
+
+    return (placeholder_sum, new_first)
+
+
+class Solver(
+    AdventDaySolver,
+    day=9,
+    year=2023,
+    name="Mirage Maintenance",
+    solution=(2005352194, 1077),
+):
+    def __init__(self, input):
+        super().__init__(input)
+        self.entries = parse_inputs(input)
+
+    def solve(self):
+        part_1 = 0
+        part_2 = 0
+
+        for e in self.entries:
+            a, b = reduce(e)
+            part_1 += a
+            part_2 += b
+
+        return (part_1, part_2)
+
+
+class Tests(AdventDayTestCase):
+    def setUp(self):
+        init_logging(logging.DEBUG)
+        super().setUp(Solver)
+
+    def test_sample_1a(self):
+        d = self._create_sample_solver("""0   3   6   9  12  15""")
+        self.assertSequenceEqual([[0, 3, 6, 9, 12, 15]], d.entries)
+
+        s = d.solve()
+
+        self.assertEqual(18, s[0])
+
+    def test_sample_1b(self):
+        d = self._create_sample_solver("""1   3   6  10  15  21""")
+        s = d.solve()
+
+        self.assertEqual(28, s[0])
+
+    def test_sample_1c(self):
+        d = self._create_sample_solver("""10  13  16  21  30  45""")
+        s = d.solve()
+
+        self.assertEqual(68, s[0])
+
+    def test_sample_2a(self):
+        d = self._create_sample_solver("""10  13  16  21  30  45""")
+        s = d.solve()
+
+        self.assertEqual(5, s[1])
+
+    def test_sample_all(self):
+        d = self._create_sample_solver(
+            """0   3   6   9  12  15
+1   3   6  10  15  21
+10  13  16  21  30  45"""
+        )
+        s = d.solve()
+
+        self.assertEqual(114, s[0])
+        self.assertEqual(2, s[1])
+
+
+if __name__ == "__main__":
+    run_tests_for_solver(unittest.TestProgram(exit=False), Solver)
