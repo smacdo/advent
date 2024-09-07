704d23ea37a0caa07392bff2d0b989ff1c6481f3
Scott MacDonald <scott@smacdo.com>
2024-09-07 13:22:47 -0700

    install advent submodules as well as advent module

--- src/advent/main.py
+++ src/advent/main.py
@@ -1,7 +1,12 @@
 # TODO: Dynamically discover solutions
 from donner.cli import cli_main
 
-import y2023.day1  # noqa: F401
+from .y2023 import day1  # noqa: F401
+
+
+def main():
+    cli_main()
+
 
 if __name__ == "__main__":
-    cli_main()
+    main()
